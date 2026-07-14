44c23f311ea4c5bcbd30c36b25c8dfa44c05ba72
Rahul Kumar Upadhyay <rahul.k.upadhyay21@gmail.com>
2026-07-14 23:14:40 +0530

    Time: 1133 ms (46.95%), Space: 292.5 MB (33.91%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    int dp[201][201][201];
+   
     int MOD=1e9+7;
     int subsequencePairCount(vector<int>& nums) {
         int n=nums.size();
         int max_val=*max_element(nums.begin(), nums.end());
-        memset(dp,0,sizeof(dp));
-        for(int i=0; i<=max_val; i++){
+        vector<vector<int>>prev(max_val+1,vector<int>(max_val+1,0));
+
+        for(int  i=0; i<=max_val; i++){
             for(int j=0; j<=max_val; j++){
                 if(i!=0 && j!=0 && i==j){
-                    dp[n][i][j]=1;
+                    prev[i][j]=1;
                 }
-                else dp[n][i][j]=0;
+                
             }
         }
+       
 
         for(int idx=n-1; idx>=0; idx--){
+            vector<vector<int>>curr(max_val+1,vector<int>(max_val+1,0));
             for(int i=max_val; i>=0; i--){
                 for(int j=max_val; j>=0; j--){
-                    int skip=dp[idx+1][i][j];
-                    int ans1=dp[idx+1][__gcd(i,nums[idx])][j];
-                    int ans2=dp[idx+1][i][__gcd(j,nums[idx])];
-                    dp[idx][i][j]=(0LL+skip+ans1+ans2)%MOD;
-
-
+                    int skip=prev[i][j];
+                    int ans1=prev[__gcd(i,nums[idx])][j];
+                    int ans2=prev[i][__gcd(j,nums[idx])];
+                    curr[i][j]=(0LL+skip+ans1+ans2)%MOD;
+                    
                 }
             }
+            prev=curr;
         }
-        return dp[0][0][0];
+        return prev[0][0];
 
 
         
