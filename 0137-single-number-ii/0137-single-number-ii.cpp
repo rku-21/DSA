1166767756a1232e68677e7a78da99ac15239496
Rahul Kumar Upadhyay <rahul.k.upadhyay21@gmail.com>
2026-07-25 11:52:37 +0530

    Time: 0 ms (100%), Space: 13.3 MB (87.26%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,19 +3,22 @@ public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
         int ans=0;
-        for(int i=0; i<32; i++){
-            int sum=0;
+        for(int bit=0; bit<32; bit++){
+            int bitsSum=0;
             for(auto num:nums){
-                if(num & (1<<i)){
-                    sum+=1;
-                }
+              if(num & (1<<bit)){
+                bitsSum+=1;
+              }
+
             }
-            int rem=sum%3;
-            if(rem==1) ans=ans | (1<<i);
-
+            int rem=bitsSum%3;
+            if(rem){
+                ans= ans | (1<<bit);
+            }
         }
-
         return ans;
+
+
         
     }
 };
