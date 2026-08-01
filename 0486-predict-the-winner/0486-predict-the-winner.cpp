2eb1274e6d17953815ca5e05690609fa5f0ca8f7
Rahul Kumar Upadhyay <rahul.k.upadhyay21@gmail.com>
2026-08-01 10:20:26 +0530

    Time: 620 ms (5.37%), Space: 10.2 MB (45.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -9,8 +9,21 @@ public:
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        // return solve(0,n-1,nums)>=0;
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<n; j++){
+                if(i>j){
+                    dp[i][j]=0;
+                    continue;
+                }
+                int left=nums[i]-solve(i+1,j,nums);
+                int right=nums[j]-solve(i,j-1,nums);
+                dp[i][j]=max(left,right);
 
-        return solve(0,n-1,nums)>=0;
+            }
+        }
+        return dp[0][n-1]>=0;
         
     }
 };
