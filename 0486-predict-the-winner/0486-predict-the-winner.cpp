923339a53abf2697d08b569ca0c01c5e63a3a211
Rahul Kumar Upadhyay <rahul.k.upadhyay21@gmail.com>
2026-08-01 10:13:36 +0530

    Time: 151 ms (7.81%), Space: 9.5 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    bool solve(int i, int j, auto&nums,int score1,int score2,bool turn){
-        if(i>j) {
-            return score1>=score2;
-        }
-        bool ans1=false;
-        bool ans2=false;
-        if(turn){
-            ans1=solve(i+1, j, nums,score1+nums[i],score2,false);
-            ans2=solve(i,j-1,nums,score1+nums[j],score2,false);
-            return ans1 || ans2;
-            
-        }
-     
-            ans1=solve(i+1, j, nums,score1,score2+nums[i],true);
-            ans2=solve(i,j-1,nums,score1,score2+nums[j],true);
+    int solve(int i, int j, auto&nums){
+        if(i>j) return 0;
 
-        
-        return ans1 && ans2;
+        int left=nums[i]-solve(i+1,j,nums);
+        int right=nums[j]-solve(i,j-1,nums);
+        return max(left,right);
     }
     bool predictTheWinner(vector<int>& nums) {
-
         int n=nums.size();
-        return solve(0,n-1,nums,0,0,true);
+
+        return solve(0,n-1,nums)>=0;
         
     }
 };
